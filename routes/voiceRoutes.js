148c6f711cf5c4ff27e7d4e18f2825c00e561532
Asad Amin Data Analyst | PowerBI Developer <asadamin236@gmail.com>
2025-07-26 09:57:15 +0500

    Updated Code

--- routes/voiceRoutes.js
+++ routes/voiceRoutes.js
@@ -4,4 +4,11 @@ const { handleVoiceToGemini } = require("../controllers/voiceController");
 
 router.post("/voice", handleVoiceToGemini);
 
+// Allow GET requests for /voice (for browser testing)
+router.get("/voice", (req, res) => {
+  res.json({
+    message: "Voice endpoint is working. Use POST for actual functionality.",
+  });
+});
+
 module.exports = router;
